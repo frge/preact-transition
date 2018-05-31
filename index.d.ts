c2a0d7262ac38822f609bb7ed1c5d0b93a6e0fbe
eros <frge@mail.com>
2018-05-31 20:50:08 +0800

    docs: 添加组件的typing声明

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+import * as preact from 'preact';
+
+export interface TransitionDone {
+  (when?: number);
+}
+
+export interface TransitionProps {
+  tag?: string;
+  name?: string;
+  css?: boolean;
+  appear?: boolean;
+  type?: string;
+  mode?: string;
+  onBeforeEnter?(el: Element);
+  onEnter?(el: Element, done: TransitionDone);
+  onAfterEnter?(el: Element, disappear?: true);
+  onBeforeLeave?(el: Element);
+  onLeave?(el: Element, done: TransitionDone);
+  onAfterLeave?(el: Element, disappear?: true);
+  [key: string]: any;
+}
+
+export class Transition extends preact.Component<TransitionProps, {}> {
+  render(props: TransitionProps, {}): JSX.Element;
+}
